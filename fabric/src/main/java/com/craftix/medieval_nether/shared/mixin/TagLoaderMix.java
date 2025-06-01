e127a7c33de9dd221eeec716701ddf712f7635de
Craft1x <31216111+Craft1x@users.noreply.github.com>
2025-06-01 19:24:53 +0300

    first commit

--- fabric/src/main/java/com/craftix/medieval_nether/shared/mixin/TagLoaderMix.java
+++ fabric/src/main/java/com/craftix/medieval_nether/shared/mixin/TagLoaderMix.java
@@ -2,17 +2,16 @@ package com.craftix.medieval_nether.shared.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-
 import net.minecraft.tags.TagLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.List;
 
-@Mixin(value = TagLoader.class, remap = true)
+@Mixin(value = TagLoader.class, remap = false) //true for fabric
 public abstract class TagLoaderMix {
     @WrapOperation(method = "build(Lnet/minecraft/tags/TagEntry$Lookup;Ljava/util/List;)Lcom/mojang/datafixers/util/Either;", at = @At(value = "INVOKE", target = "Ljava/util/List;isEmpty()Z"))
-    private boolean resolveAll(List instance, Operation<Boolean> original) {
+    private boolean wrapOperation(List instance, Operation<Boolean> original) {
         return true;
     }
 }
---
+++ neoforge/src/main/java/com/craftix/medieval_nether/MainEntrypointNeoforge.java
@@ -0,0 +1,14 @@
+package com.craftix.medieval_nether;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(MainEntrypointNeoforge.MODID)
+public class MainEntrypointNeoforge {
+    public static final String MODID = "medieval_nether";
+
+    public MainEntrypointNeoforge(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
